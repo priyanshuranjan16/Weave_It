ca5c46173961d60d1e09dd67e9848b9bc92ad097
Priyanshu Ranjan <priyanshu.2022ug2005@iiitranchi.ac.in>
2026-01-28 00:15:43 +0530

    added collective page

---
+++ client/src/app/collective/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/sections/Navbar";
+import Footer from "@/components/sections/Footer";
+
+export default function CollectivePage() {
+    return (
+        <main className="w-full min-h-screen bg-black flex flex-col items-center justify-center overflow-hidden">
+            {/* <Navbar/> */}
+            
+            <Footer />
+        </main>
+    );
+}
