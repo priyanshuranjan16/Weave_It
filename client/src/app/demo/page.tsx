0cd57c510894aeab1937cabd63277dcd3d4b1827
Priyanshu Ranjan <priyanshu.2022ug2005@iiitranchi.ac.in>
2026-01-30 07:27:55 +0530

    built demo page

---
+++ client/src/app/demo/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function DemoPage() {
+    return (
+        <main
+            className="h-screen w-full bg-center bg-no-repeat"
+            style={{
+                backgroundImage: "url('https://images.typeform.com/images/vDBLGfjmr6jj/image/default-firstframe.png')",
+                backgroundSize: '100% 100%',
+            }}
+        >
+        </main>
+    );
+}
